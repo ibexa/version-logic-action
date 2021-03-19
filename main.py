7a29b09cb79c5fdb5d65f3032f72f496e68a979f
Petro Kurbatskyi <petro.kurbatskyi@ibexa.co>
2021-03-19 13:31:44 +0100

    EZP-32364: Add Version Logic action

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import os
 import requests  # noqa We are just importing this to prove the dependency installed correctly
+import semver
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    # cut initial v from input
+    current_tag = os.environ["INPUT_CURRENTTAG"][1:]
 
-    my_output = f"Hello {my_input}"
+    major, minor, patch, _, _ = semver.VersionInfo.parse(current_tag)
+    if not patch and not minor:
+        previous_tag = semver.format_version(major - 1, 0, 0)
+    elif not patch:
+        previous_tag = semver.format_version(major, minor - 1, 0)
+    else:
+        previous_tag = semver.format_version(major, minor, patch - 1)
 
-    print(f"::set-output name=myOutput::{my_output}")
+    print(f"::set-output name=previousTag::v{previous_tag}")
 
 
 if __name__ == "__main__":
